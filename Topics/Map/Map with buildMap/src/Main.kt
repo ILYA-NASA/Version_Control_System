3c0336a1545f62123b2f846ad51f46ade90670e2
ILYA.NASA <il.nasa@outlook.com>
2023-03-22 15:28:12 +0300

    used BUILDMAP function

---
+++ Topics/Map/Map with buildMap/src/Main.kt
@@ -0,0 +1,6 @@
+val map = buildMap<String, String> {
+    put("Apple", "Green")
+    put("Banana", "Yellow")
+    put("Strawberry", "Red")
+//    putAll(mapOf("Apple" to "Green", "Banana" to "Yellow", "Strawberry" to "Red"))
+}
