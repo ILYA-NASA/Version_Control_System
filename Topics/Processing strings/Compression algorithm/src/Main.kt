eb99ddef3a25b87cfdadd0b1173b9ae3c5a42263
ILYA.NASA <il.nasa@outlook.com>
2023-05-19 10:56:06 +0300

    used a lot of If expressions (very interesting task)

--- Topics/Processing strings/Compression algorithm/src/Main.kt
+++ Topics/Processing strings/Compression algorithm/src/Main.kt
@@ -1,3 +1,28 @@
 fun main() {
-    // write your code here    
+    // write your code here
+    val inputString = readln()
+    var countRepeatedIt = 1
+    var currentIt = inputString.first()
+    val forEachString =
+        if (inputString.length != 1) {
+            inputString.substring(1)
+        } else {
+            print(currentIt + "1")
+            return
+        }
+    var currentLengthString = 0
+
+    forEachString.forEach {
+        currentLengthString++
+        if (it == currentIt) {
+            countRepeatedIt++
+        } else {
+            print("$currentIt$countRepeatedIt")
+            countRepeatedIt = 1
+            currentIt = it
+        }
+        if (currentLengthString == forEachString.length) {
+            print("$currentIt$countRepeatedIt")
+        }
+    }
 }
