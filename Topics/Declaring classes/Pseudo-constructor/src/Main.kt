1a6958c701e73b0698f3751839201f2ebd050bfc
ILYA.NASA <il.nasa@outlook.com>
2023-06-20 10:48:47 +0300

    used also lambda with class

--- Topics/Declaring classes/Pseudo-constructor/src/Main.kt
+++ Topics/Declaring classes/Pseudo-constructor/src/Main.kt
@@ -4,6 +4,14 @@ class Point3D {
     var z: Int = 0
 }
 
+/*
 fun createPoint(x: Int, y: Int, z: Int): Point3D {
-    // provide implementation here
+    val point = Point3D()
+    point.x = x
+    point.y = y
+    point.z = z
+    return point
 }
+*/
+
+fun createPoint(x: Int, y: Int, z: Int) = Point3D().also { it.x = x; it.y = y; it.z = z }
