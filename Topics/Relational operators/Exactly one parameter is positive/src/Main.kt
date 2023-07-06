79d0a3e3c632ac45d9bbcf95c270e5532b52adc7
ILYA.NASA <il.nasa@outlook.com>
2023-07-06 10:42:05 +0300

    used filter, size and let methods for Array

--- Topics/Relational operators/Exactly one parameter is positive/src/Main.kt
+++ Topics/Relational operators/Exactly one parameter is positive/src/Main.kt
@@ -1,9 +1,4 @@
 const val TASK_INPUT_COUNT = 3
 const val TASK_POSITIVE_COUNT = 1
 
-fun main() {
-    val inputArray = IntArray(TASK_INPUT_COUNT) { readln().toInt() }
-    var count = 0
-    inputArray.forEach { if (it > 0) count++ }
-    println(count == TASK_POSITIVE_COUNT)
-}
+fun main() = (Array(TASK_INPUT_COUNT) { readln().toInt() }.filter { it > 0 }.size == TASK_POSITIVE_COUNT).let(::println)
